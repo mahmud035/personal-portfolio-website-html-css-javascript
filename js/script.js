9705675ae57466e9c3cd03ac4ea96e0f772415c1
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-04 22:12:30 +0600

    added sticky navgition effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const header = document.querySelector('header');
 
+//Navigation bar effects on scroll
+window.addEventListener('scroll', function () {
+  header.classList.toggle('sticky', window.scrollY > 0);
+
+  // INFO:The scrollY property returns the pixels a document has scrolled from the upper left corner of the window.
+});
