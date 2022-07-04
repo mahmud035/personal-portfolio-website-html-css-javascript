e22c1aceae7e3b95bcf4d71485b2df65007160bf
mahmud035 <mahamudulhasan4148@gmail.com>
2022-07-05 02:04:20 +0600

    added scrollreveal

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ window.addEventListener('scroll', function () {
 
   // INFO:The scrollY property returns the pixels a document has scrolled from the upper left corner of the window.
 });
+
+const menu = document.querySelector('#menu-icon');
+const navList = document.querySelector('.nav-list');
+
+menu.onclick = () => {
+  menu.classList.toggle('bx-x');
+  navList.classList.toggle('active');
+};
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navList.classList.remove('active');
+};
+
+const sr = ScrollReveal({
+  distance: '45px',
+  duration: 2700,
+  reset: true,
+});
+
+sr.reveal('.home-text', { delay: 350, origin: 'left' });
+sr.reveal('.home-img', { delay: 350, origin: 'right' });
+
+sr.reveal(
+  '.sub-service, .about, .portfolio, .service, .complete-task,.contact',
+  { delay: 200, origin: 'bottom' }
+);
